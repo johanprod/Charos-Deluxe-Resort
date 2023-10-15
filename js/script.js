ae044456e754a658897372a9aa6a3b0cd6219b39
johanprod <134876657+johanprod@users.noreply.github.com>
2023-10-15 16:18:25 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+function scrollValue() {
+  var navbar = document.getElementById('navbar');
+  var scroll = window.scrollY;
+  if (scroll < 100) {
+      navbar.classList.remove('BgColor');
+  } else {
+      navbar.classList.add('BgColor');
+  }
+}
+
+window.addEventListener('scroll', scrollValue);
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
